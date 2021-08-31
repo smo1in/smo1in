8569cc44f11b91f51cd70b32d34a26d36010b732
smo1in <smolingm@gmail.com>
2021-08-31 13:25:54 +0300

    forma

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function proceed () {
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media');
+    form.style.display = 'hidden';
+    document.body.appendChild(form)
+    form.submit();
+}
