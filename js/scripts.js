f881939cd62f3f005acb05fb13e63cca0b09069e
smo1in <smolingm@gmail.com>
2023-04-21 20:53:59 +0700

    last form suka!

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function proceed () {
-    var form = document.createElement('form');
-    form.setAttribute('method', 'post');
-    form.setAttribute('action', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media');
-    form.style.display = 'hidden';
-    document.body.appendChild(form)
-    form.submit();
-}
-
